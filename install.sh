67b2b4af2ea6e57a7e8f437f8fff5c96c74e6da8
Thenujan <sthenujan2002@gmail.com>
2021-12-28 20:18:26 +0530

    empty

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-pkexec sh -c 'mkdir -p /usr/share/grub_editor/ && cp main.py /usr/share/grub_editor/main.py && cp  main.ui /usr/share/grub_editor/main.ui
+pkexec sh -c 'mkdir -p /usr/share/grub_editor/ && cp * /usr/share/grub_editor/ && cp  main.ui /usr/share/grub_editor/main.ui
