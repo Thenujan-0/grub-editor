f418b0b6f9237f1d790384d39623f9851a9a83e4
Thenujan <sthenujan2002@gmail.com>
2022-04-17 14:05:34 +0530

    added two methods remove_btn_cancel,add_btn_cancel

--- widgets/dialog.py
+++ widgets/dialog.py
@@ -1,11 +1,15 @@
 from PyQt5 import QtWidgets ,uic
 from PyQt5.QtWidgets import QDesktopWidget
 import os 
+import sys
 
 
 #remove /widgets part from path
 PATH = os.path.dirname(os.path.realpath(__file__))
 PATH = PATH[0:-8]
+
+sys.path.append(PATH)
+
 
 class DialogUi(QtWidgets.QDialog):
     def __init__(self,btn_cancel=True):
@@ -34,6 +38,16 @@ class DialogUi(QtWidgets.QDialog):
         """ Decides on showing the dialog using the preferences file and the value of label that is set """
         
         self.show()
+    
+    def remove_cancel_btn(self):
+        self.horizontalLayout.takeAt(0).widget().deleteLater()
+        self.btn_cancel.setParent(None)
+    
+    def add_cancel_btn(self):
+        self.btn_cancel=QtWidgets.QPushButton()
+        self.btn_cancel.setText("Cancel")
+        self.horizontalLayout.insertWidget(0,self.btn_cancel)
+        self.btn_cancel.clicked.connect(self.btn_cancel_callback)
 
 
 def main():
