cc6847550523b20b876eb109d0e5f38b411ccb1e
Thenujan <sthenujan2002@gmail.com>
2022-04-17 14:02:24 +0530

    added QSpacerItem handling to insert into

--- libs/qt_functools.py
+++ libs/qt_functools.py
@@ -1,6 +1,6 @@
 
 ''' Some functions that are usefull in qt gui applications '''
-
+from PyQt5 import QtWidgets
 
 def reconnect(signal ,new_handler=None,old_handler=None):
     try:
@@ -18,11 +18,26 @@ def reconnect(signal ,new_handler=None,old_handler=None):
 def insert_into(layout,index,widget):
     items=[]
     for i in reversed(range(index,layout.count())):
-        item =layout.itemAt(i).widget()
-        item.setParent(None)
-        items.append(item)
-        
-    # printer('widget',widget)
+        item = layout.takeAt(i)
+        if isinstance(item,QtWidgets.QWidgetItem):
+            widget_ =item.widget()
+            widget_.setParent(None)
+            items.append(item)
+        elif isinstance(item,QtWidgets.QSpacerItem):
+            if  item.sizePolicy().horizontalPolicy()==7:
+                horizontalSpacer = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Expanding,\
+                    QtWidgets.QSizePolicy.Minimum) 
+                items.append(horizontalSpacer)
+            else:
+                raise Exception("Error in insert_into")
+        else:
+            raise Exception("Non handled case in insert_into")
     layout.addWidget(widget)
     for i in reversed(range(len(items))):
-        layout.addWidget(items[i])
+        item = items[i]
+        if isinstance(item,QtWidgets.QWidgetItem):
+            layout.addWidget(item.widget())
+        elif isinstance(item,QtWidgets.QSpacerItem):
+            layout.addItem(item)
+        else:
+            raise Exception("Non handled case in insert_into")
