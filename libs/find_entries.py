d71cf33ce337d89bbe7da4fd0a669d3b535ecf5d
Thenujan Sandramohan <sthenujan2002@gmail.com>
2022-07-18 14:15:49 +0530

    made some improvements to error handling

--- libs/find_entries.py
+++ libs/find_entries.py
@@ -1,7 +1,13 @@
-cmd_find_entries=["awk -F\\' '$1==\"menuentry \" || $1==\"submenu \" {print i++ \" : \" $2}; /\\tmenuentry / {print \"\\t\" i-1\">\"j++ \" : \" $2};' /boot/grub/grub.cfg"]
+"""This module is used to find the entries in the grub config file"""
+
 import traceback
 import subprocess
-out =subprocess.getoutput(cmd_find_entries)
+
+
+class GrubConfigNotFound(Exception):
+    """ Raised when /boot/grub/grub.cfg is not found """
+
+
 class MainEntry():
     parent=None
     title:str
@@ -9,6 +15,19 @@ class MainEntry():
     def __init__(self,title,sub_entries_):
         self.title = title
         self.sub_entries=sub_entries_
+        
+    def __repr__(self) -> str:
+        to_return="" 
+        if len(self.sub_entries)==0:
+            return "MainEntry(title:'"+self.title+"')"
+        
+        for sub_entry in self.sub_entries:
+            to_return+="\n"+sub_entry.title
+
+        
+        to_return="MainEntry(title:'"+self.title+"', sub_entries :["+to_return+"])"
+            
+        return to_return
         
     def set_parents_for_children(self):
         for child in self.sub_entries:
@@ -21,40 +40,56 @@ class MainEntry():
         print(self.title)
         
         print('printing sub_entries _____________')
-        for i in range(len(self.sub_entries)):
-            print(self.sub_entries[i].title,'title of sub entry')
-            print(self.sub_entries[i].parent,'parent of sub entry')
+        for sub_entry in self.sub_entries:
+            print(sub_entry.title,'title of sub entry')
+            print(sub_entry.parent,'parent of sub entry')
         print('finished printing for one big main entry')
         print('----------------')
         print('----------------')
         print('')
-main_entries=[]
-
-lines =out.splitlines()
-for i in range(len(lines)):
-    
-    if lines[i][0].isdigit():
-        to_append=MainEntry(lines[i][4:],[])
-        main_entries.append(to_append)
         
-    else:
-        try:
-            to_append=MainEntry(lines[i][7:],[])
-            main_entries[-1].sub_entries.append(to_append) 
-        except Exception as e:
-            print(traceback.format_exc())
-            print(e)
-            print('error occured as an entry that was thought to be a sub entry couldnt be added to last main entry on the list .\
-                  Error might have occured because the main_entries list is empty')
-            print('--------------------------Printing the output of the command to find entries--------------------------------------')
-            print(out)
+def find_entries():
+    cmd_find_entries=["awk -F\\' '$1==\"menuentry \" || $1==\"submenu \" \
+    {print i++ \" : \" $2}; /\\tmenuentry / {print \"\\t\" i-1\">\"j++ \" :\
+        \" $2};' /boot/grub/grub.cfg"]
+    
+    out =subprocess.getoutput(cmd_find_entries)
+    NO_SUCH_FILE_ERR_MSG="awk: fatal: cannot open file `/boot/grub/grub.cfg' for reading: No such file or directory"
+
+    #if NO_SUCH_FILE_ERR_MSG in out:
+    #raise GrubConfigNotFound
+    
+    main_entries=[]
+    lines =out.splitlines()
+    for line in lines:
+        
+        if line[0].isdigit():
+            to_append=MainEntry(line[4:],[])
+            main_entries.append(to_append)
+            
+        else:
+            try:
+                to_append=MainEntry(line[7:],[])
+                main_entries[-1].sub_entries.append(to_append) 
+            except IndexError as e:
+                print(traceback.format_exc())
+                print(e)
+                print('error occured as an entry that was thought to be a sub entry couldnt be added to last main entry on the list .\
+                    Error might have occured because the main_entries list is empty')
+                print('--------------------------Printing the output of the command to find entries--------------------------------------')
+                print(out)
+                print("printing main entries",main_entries)
+                print("printing to_append",to_append)
+                print("line being parsed",line)
 
 
 
-for entry in main_entries:
-    entry.set_parents_for_children()
+    for entry in main_entries:
+        entry.set_parents_for_children()
+        
+    return main_entries
     
     
 if __name__=="__main__":
-    print(main_entries)
+    print(find_entries())
     
